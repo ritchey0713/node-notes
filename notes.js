8bde47d1355d979e430e41e951e926089b08d641
Dj <ritchey0713@gmail.com>
2020-01-22 16:33:18 -0500

    add functions

--- notes.js
+++ notes.js
@@ -1,5 +1,34 @@
+const fs = require("fs")
+
 const getNotes = () => {
   return "All notes"
 }
 
-module.exports = getNotes
+const addNote = (title, body) => {
+  const notes = loadNotes()
+  notes.push({
+    title,
+    body
+  })
+  saveNotes(notes)
+}
+
+const saveNotes = (notes) => {
+  const dataJSON = JSON.stringify(notes)
+  fs.writeFileSync("notes.json", dataJSON)
+}
+
+const loadNotes = () => {
+  try {
+    const dataBuffer = fs.readFileSync('notes.json')
+    const dataJSON = dataBuffer.toString()
+    return JSON.parse(dataJSON)
+  } catch (e) {
+    return []
+  }
+}
+
+module.exports = {
+  getNotes,
+  addNote
+}
