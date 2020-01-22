681cda57160e3c35bb69c37c9bf72aa1cc79a19e
Dj <ritchey0713@gmail.com>
2020-01-22 14:41:16 -0500

    use yargs

--- app.js
+++ app.js
@@ -1,3 +1,45 @@
-const validator = require("validator")
+const chalk = require("chalk")
 
-console.log(validator.isEmail("andrew@test.com"))
+const yargs = require('yargs')
+
+// console.log(process.argv)
+
+yargs.version('1.1.0')
+
+
+//create add command 
+
+yargs.command({
+  command: 'add',
+  describe: "add a new note",
+  handler: function () {
+    console.log("adding note...")
+  }
+})
+
+yargs.command({
+  command: "remove",
+  describe: "remove note",
+  handler: function () {
+    console.log('remove note')
+  }
+})
+
+yargs.command({
+  command: "list",
+  describe: "list all notes",
+  handler: function () {
+    console.log('list of notes')
+  }
+})
+
+yargs.command({
+  command: "read",
+  describe: "read note",
+  handler: function () {
+    console.log('read note')
+  }
+})
+
+
+console.log(yargs.argv)
